1b0ad1815bc62c5bf91ed45ba18aa9962566d66c
Lvtao <Lv-tao@outlook.com>
2018-11-04 21:35:37 +0800

    新增首页轮播图

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for='item of swiperList' :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg'
+      },
+      {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/f1/e57bc93226317102.jpg_750x200_9ab37bd0.jpg'
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background-color:#fff
+.wrapper
+  overflow:hidden
+  width:100%
+  height:0
+  padding-bottom:30%
+  .swiper-img
+    width:100%
+</style>
