b9335b3c0909d4679498a96139c1c711b43d78b2
Lvtao <Lv-tao@outlook.com>
2018-11-05 15:58:02 +0800

    完善首页

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="icons">
-    <swiper>
+    <swiper :options="swiperOption">
       <swiper-slide v-for="(page,index) of pages" :key="index">
         <div class="icon" v-for="item of page" :key="item.id">
           <div class="icon-img">
@@ -9,6 +9,7 @@
           <p class="icon-desc">{{item.desc}}</p>
         </div>
       </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
   </div>
 </template>
@@ -18,6 +19,9 @@ export default {
   name: 'HomeIcons',
   data () {
     return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      },
       iconList: [
         {
           id: '#001',
@@ -93,34 +97,36 @@ export default {
 @import '~styles/mixins.styl'
 .icons >>>.swiper-container
   height: 0
-  padding-bottom: 42.67%
-.icon
-  overflow hidden
-  position relative
-  float left
-  width 25%
-  height 0
-  padding-bottom 21.34%
-  .icon-img
-    position absoute
-    top 0
-    left 0
-    right 0
-    box-sizing border-box
-    padding .1rem
-    bottom .44rem
-    .icon-imgContent
-      display block
-      margin 0 auto
-      width 1.1rem
-  .icon-desc
-    position absolute
-    left 0
-    right 0
-    bottom 0
-    height .44rem
-    line-height .44rem
-    text-align center
-    color $darkTextColor
-    ellipsis()
+  padding-bottom: 50%
+.icons
+  margin-top .1rem
+  .icon
+    overflow hidden
+    position relative
+    float left
+    width 25%
+    height 0
+    padding-bottom 21.34%
+    .icon-img
+      position absoute
+      top 0
+      left 0
+      right 0
+      box-sizing border-box
+      padding .1rem
+      bottom .44rem
+      .icon-imgContent
+        display block
+        margin 0 auto
+        width 70%
+    .icon-desc
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      height .35rem
+      line-height .35rem
+      text-align center
+      color $darkTextColor
+      ellipsis()
 </style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item">
+        <img class="item-img" src="http://img1.qunarzz.com/sight/p0/1602/de/de8400021b664c5390.img.jpg_200x200_46b16057.jpg" alt="">
+        <div class="item-info">
+          <p class="item-title">的设等数据库计费</p>
+          <p class="item-desc">大会上尽快发货时的授课方式可发货速度快黑客技术</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .recommend-title
+    line-height .8rem
+    background-color #eee
+    text-indent .2rem
+    .item
+      overflow hidden
+      display flex
+      height 1.9rem
+      background red
+    .item-img
+      width 1rem
+      height 1rem
+      padding .1rem
+</style>
