9148e44cb5195e3b49b3eabe93f078eff56573ae
Lvtao <Lv-tao@outlook.com>
2018-11-06 10:13:05 +0800

    周末游组件

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom">
+        <div class="item-img-wrapper">
+          <img class="item-img" src="http://img1.qunarzz.com/sight/source/1511/14/1cf5dc2ecbfc16.jpg_r_640x214_b4b3a58a.jpg"/>
+        </div>
+        <div class="item-info">
+          <p class="item-title">的设等数据库计费</p>
+          <p class="item-desc">大会上尽快dfd就可获得会计核算房价肯定会借款方gg发货时发货速度快黑客技术</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+  .title
+    line-height .8rem
+    background-color #eee
+    text-indent .2rem
+  .item-img-wrapper
+    overflow hidden
+    height 0
+    padding-bottom 33.9%
+  .item-img
+    width 100%
+  .item-info
+    padding .1rem
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color #ccc
+      ellipsis()
+</style>
