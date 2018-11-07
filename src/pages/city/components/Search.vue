b4960c43b0e1afd333663b254ad1f94d6f4ad7ef
Lvtao <Lv-tao@outlook.com>
2018-11-07 11:07:18 +0800

    city-search finished

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input class="searchInput" type="text" placeholder="请输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+  .search
+    height .72rem
+    padding 0 .1rem
+    background-color $bgColor
+    .searchInput
+      width 100%
+      height .62rem
+      line-height .62rem
+      text-align center
+      padding 0 .1rem
+      box-sizing border-box
+      font-size .3rem
+      color #666
+      border-radius .06rem
+</style>
