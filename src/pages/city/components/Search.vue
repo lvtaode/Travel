5ba0201cea858b329ad86f86d10609df6daa44e4
Lvtao <Lv-tao@outlook.com>
2018-11-08 20:04:01 +0800

    完成城市搜索

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,66 @@
 <template>
-  <div class="search">
-    <input class="searchInput" type="text" placeholder="请输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="searchInput" type="text" placeholder="请输入城市名或拼音">
+    </div>
+    <div class="serach-content" v-show="keyword" ref='search'>
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">
+          {{item.name}}
+        </li>
+        <li class="search-item border-bottom" v-show="hasNoData">
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  props: {
+    cities: Object
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 ||
+            value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -26,4 +80,18 @@ export default {
       font-size .28rem
       color #666
       border-radius .06rem
+  .serach-content
+    z-index 1
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    background #eee
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      color #666
+      background #fff
 </style>
