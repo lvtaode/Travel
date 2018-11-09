68ce5f514e00e871922d015f35cbeeccc3d42ffc
Lvtao <Lv-tao@outlook.com>
2018-11-09 14:58:24 +0800

    jk

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
