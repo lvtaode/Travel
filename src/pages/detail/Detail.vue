e2c9a064e31d53ea839a95a61d86462d72ea6cfd
Lvtao <Lv-tao@outlook.com>
2018-11-11 14:09:58 +0800

    detail-ajax finished

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <detail-banner></detail-banner>
+    <detail-banner :sightName="sightName"
+                   :bannerImg="bannerImg"
+                   :gallaryImgs="gallaryImgs">
+    </detail-banner>
     <detail-header></detail-header>
     <div class="content">
       <detail-list :list="list"></detail-list>
@@ -12,6 +15,7 @@
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
 import DetailList from './components/List'
+import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
@@ -21,31 +25,33 @@ export default {
   },
   data () {
     return {
-      list: [
-        {
-          title: '成人票',
-          children: [{
-            title: '成人三馆联票',
-            children: [{
-              title: '成人三馆联票 - 学生票'
-            }, {
-              title: '成人三馆联票 - 儿童票'
-            }]
-          },
-          {
-            title: '成人五馆联票'
-          }]
-        },
-        {
-          title: '学生票'
-        },
-        {
-          title: '儿童票'
-        },
-        {
-          title: '特惠票'
+      list: [],
+      sightName: '',
+      gallaryImgs: [],
+      bannerImg: ''
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
+  },
+  methods: {
+    getDetailInfo () {
+      console.log(this.$route.params.id)
+      axios.get('api/detail.json', {
+        params: {
+          id: this.$route.params.id
         }
-      ]
+      }).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      console.log(res)
+      if (res.ret && res.data) {
+        this.sightName = res.data.sightName
+        this.bannerImg = res.data.bannerImg
+        this.gallaryImgs = res.data.gallaryImgs
+        this.list = res.data.categoryList
+      }
     }
   }
 }
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <div class="banner" @click="handleBannerClick">
-      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" alt="">
+      <img class="banner-img" :src="bannerImg" alt="">
       <div class="banner-info">
-        <div class="banner-title">故宫(AAAAA)景区</div>
+        <div class="banner-title">{{sightName}}</div>
         <div class="banner-number"><span class="iconfont banner-icon">&#xe674;</span>39</div>
       </div>
     </div>
-    <common-gallary :imgs="imgs"
+    <common-gallary :imgs="gallaryImgs"
                     v-show="showGallary"
                     @close="handleGallaryClose">
     </common-gallary>
@@ -21,10 +21,14 @@ export default {
   components: {
     CommonGallary
   },
+  props: {
+    sightName: String,
+    bannerImg: String,
+    gallaryImgs: Array
+  },
   data () {
     return {
-      showGallary: false,
-      imgs: ['http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_r_800x800_6edd8174.jpg', 'http://img1.qunarzz.com/sight/p0/1410/e3/73da8d3e19cdc41c1932d4fcd22ec792.water.jpg_r_800x800_7f96ccf9.jpg']
+      showGallary: false
     }
   },
   methods: {
