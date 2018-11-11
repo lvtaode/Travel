923a3a89563f83feacea86aca4b90a0787c40be0
Lvtao <Lv-tao@outlook.com>
2018-11-11 13:25:55 +0800

    detail-list finished

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,22 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   },
   data () {
     return {
@@ -21,7 +25,12 @@ export default {
         {
           title: '成人票',
           children: [{
-            title: '成人三馆联票'
+            title: '成人三馆联票',
+            children: [{
+              title: '成人三馆联票 - 学生票'
+            }, {
+              title: '成人三馆联票 - 儿童票'
+            }]
           },
           {
             title: '成人五馆联票'
--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -69,6 +69,7 @@ export default {
     color #fff
 .header-fixed
   position fixed
+  z-index 2
   top 0
   right 0
   left 0
